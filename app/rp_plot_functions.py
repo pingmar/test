25d500bf1cd0317cb1686df3cffb5f70b5f3dfb8
pingmar <141325977+pingmar@users.noreply.github.com>
2024-01-30 13:35:38 +0200

    Update rp_plot_functions.py

--- app/rp_plot_functions.py
+++ app/rp_plot_functions.py
@@ -7,6 +7,7 @@ import streamlit as st
 def make_graph(ids, ff, padding=[0,0,0,0]):
   COLOR_U = 'blue'
   COLOR_N = 'orange'
+  SIZE = 1
   pitch = VerticalPitch(goal_type='box', half=True, pad_left = padding[0], pad_right = padding[1], pad_top = padding[2], pad_bottom = padding[3])
   fig, axs = pitch.grid(figheight=8, endnote_height=0,
                         title_height=0.1, title_space=0.02,
@@ -18,8 +19,8 @@ def make_graph(ids, ff, padding=[0,0,0,0]):
       df_shot_event = events[events.id == ID].dropna(axis=1, how='all').copy()
       team1 = df_shot_event.team_name.iloc[0]
       team2 = list(set(events.team_name.unique()) - {team1})[0]
-      COLOR_1 = 'blue' if team1 == 'Ukraine' else 'orange'
-      COLOR_2 = 'blue' if team2 == 'Ukraine' else 'orange'
+      COLOR_1 = COLOR_U if team1 == 'Ukraine' else COLOR_N
+      COLOR_2 = COLOR_U if team1 == 'Ukraine' else COLOR_N
       df_team1 = df_freeze_frame[df_freeze_frame.teammate == True]
       df_team2 = df_freeze_frame[df_freeze_frame.teammate == False]
       pitch.goal_angle(df_shot_event.x, df_shot_event.y, ax=axs['pitch'], alpha=0.2, zorder=1.1,
@@ -35,8 +36,8 @@ def make_graph(ids, ff, padding=[0,0,0,0]):
       df_shot_event = events[events.id == ID].dropna(axis=1, how='all').copy()
       team1 = df_shot_event.team_name.iloc[0]
       team2 = list(set(events.team_name.unique()) - {team1})[0]
-      COLOR_1 = 'blue' if team1 == 'Ukraine' else 'orange'
-      COLOR_2 = 'blue' if team2 == 'Ukraine' else 'orange'
+      COLOR_1 = COLOR_U if team1 == 'Ukraine' else COLOR_N
+      COLOR_2 = COLOR_U if team1 == 'Ukraine' else COLOR_N
       pitch.scatter(df_shot_event.x, df_shot_event.y, c = COLOR_1, marker='football',
                       s=600, ax=axs['pitch'], label='Shooter', zorder=1.2)
       pitch.scatter(df_shot_event.end_x, df_shot_event.end_y, s=600, c=COLOR_1, label='Reciver', ax=axs['pitch'])
