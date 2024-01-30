ea22951ce98bac93aaecbbf1c9d86a87fba9de82
pingmar <141325977+pingmar@users.noreply.github.com>
2024-01-30 14:15:55 +0200

    Update rp_plot_functions.py

--- app/rp_plot_functions.py
+++ app/rp_plot_functions.py
@@ -17,6 +17,7 @@ def make_graph(ids, ff, padding=[0,0,0,0]):
     if tp:
       df_freeze_frame = freeze[freeze.id == ID].copy()
       df_shot_event = events[events.id == ID].dropna(axis=1, how='all').copy()
+      df_freeze_frame = df_freeze_frame.merge(df_lineup, how='left', on='player_id')
       team1 = df_shot_event.team_name.iloc[0]
       team2 = list(set(events.team_name.unique()) - {team1})[0]
       COLOR_1 = COLOR_U if team1 == 'Ukraine' else COLOR_N
@@ -25,23 +26,27 @@ def make_graph(ids, ff, padding=[0,0,0,0]):
       df_team2 = df_freeze_frame[df_freeze_frame.teammate == False]
       pitch.goal_angle(df_shot_event.x, df_shot_event.y, ax=axs['pitch'], alpha=0.2, zorder=1.1,
                   color='#cb5a4c', goal='right')
-      sc1 = pitch.scatter(df_team1.x, df_team1.y, s=600, c=COLOR_1, label='Attacker', ax=axs['pitch'])
-      sc2 = pitch.scatter(df_team2.x, df_team2.y, s=600, c=COLOR_2, label='Defender', ax=axs['pitch'])
-      sc3 = pitch.scatter(df_shot_event.x, df_shot_event.y, c=COLOR_1, marker='football',
+      pitch.scatter(df_team1.x, df_team1.y, s=600, c=COLOR_1, label='Attacker', ax=axs['pitch'])
+      pitch.scatter(df_team2.x, df_team2.y, s=600, c=COLOR_2, label='Defender', ax=axs['pitch'])
+      pitch.scatter(df_shot_event.x, df_shot_event.y, c=COLOR_1, marker='football',
                       s=600, ax=axs['pitch'], label='Shooter', zorder=1.2)
-      line = pitch.lines(df_shot_event.x, df_shot_event.y,
+      pitch.lines(df_shot_event.x, df_shot_event.y,
                     df_shot_event.end_x, df_shot_event.end_y, comet=True,
                     label='shot', color='#cb5a4c', ax=axs['pitch'])
+      for i, label in enumerate(df_freeze_frame.jersey_number):
+        pitch.annotate(label, (df_freeze_frame.x[i], df_freeze_frame.y[i]),
+                   va='center', ha='center', color='white',
+                   fontproperties=robotto_regular.prop, fontsize=15, ax=axs['pitch'])
     else:
       df_shot_event = events[events.id == ID].dropna(axis=1, how='all').copy()
       team1 = df_shot_event.team_name.iloc[0]
       team2 = list(set(events.team_name.unique()) - {team1})[0]
       COLOR_1 = COLOR_U if team1 == 'Ukraine' else COLOR_N
       COLOR_2 = COLOR_U if team2 == 'Ukraine' else COLOR_N
-      sc1 = pitch.scatter(df_shot_event.x, df_shot_event.y, c = COLOR_1, marker='football',
+      pitch.scatter(df_shot_event.x, df_shot_event.y, c = COLOR_1, marker='football',
                       s=600, ax=axs['pitch'], label='Shooter', zorder=1.2)
-      sc2 = pitch.scatter(df_shot_event.end_x, df_shot_event.end_y, s=600, c=COLOR_1, label='Reciver', ax=axs['pitch'])
-      line = pitch.lines(df_shot_event.x, df_shot_event.y,
+      pitch.scatter(df_shot_event.end_x, df_shot_event.end_y, s=600, c=COLOR_1, label='Reciver', ax=axs['pitch'])
+      pitch.lines(df_shot_event.x, df_shot_event.y,
                     df_shot_event.end_x, df_shot_event.end_y, comet=True,
                     label='shot', color='#cb5a4c', ax=axs['pitch'])
   axs['title'].text(0.5, 0.5, f'ABC',
@@ -50,9 +55,15 @@ def make_graph(ids, ff, padding=[0,0,0,0]):
                     fontsize=25)
   st.pyplot(fig)
 
+COLOR_U = 'blue'
+COLOR_N = 'orange'
+SIZE = 1
 competition_id=55
 season_id=43
 match_id=3788746
 parser = Sblocal()
-PATH = f'app/data/json/events/3788746.json'
-events, related, freeze, tactics = parser.event(PATH)
+PATH_EVENT = f'app/data/json/events/3788746.json'
+PATH_LINEUP = f'app/data/json/lineups/3788746.json'
+events, related, freeze, tactics = parser.event(PATH_EVENT)
+lineup = parser.(PATH_LINEUP)
+df_lineup = lineup[['player_id', 'jersey_number', 'team_name']].copy()
