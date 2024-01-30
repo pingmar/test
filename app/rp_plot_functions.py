17426f5f8d01c3a2999db1bcb086db0f611a9e18
pingmar <141325977+pingmar@users.noreply.github.com>
2024-01-30 18:10:07 +0200

    Update rp_plot_functions.py

--- app/rp_plot_functions.py
+++ app/rp_plot_functions.py
@@ -33,7 +33,7 @@ def make_graph(ids, ff, padding=[None,None,None,None]):
       for i, (label, name) in enumerate(zip(df_freeze_frame.jersey_number, df_freeze_frame.player_name)):
         pitch.annotate(label, (df_freeze_frame.x[i], df_freeze_frame.y[i]),
                    va='center', ha='center', color='white', fontsize=15, ax=axs['pitch'])
-        pitch.text(80+2*i, 20, f'{label}: {name}', va='center', ha='center', ax=axs['pitch'])
+        pitch.text(50+2*i, -20, f'{label}: {name}', va='center', ha='center', ax=axs['pitch'])
     else:
       df_shot_event = events[events.id == ID].dropna(axis=1, how='all').copy()
       team1 = df_shot_event.team_name.iloc[0]
